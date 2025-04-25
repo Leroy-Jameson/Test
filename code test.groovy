5e11bbf56f96f79a24bb0908c46b260eca20ae28
lgevry2031 <lgevry2031@localhost>
2025-04-25 08:32:30 -0400

    Creating workflow

--- code test.groovy
+++ code test.groovy
@@ -1,10 +1,15 @@
 import eu.mihosoft.vrl.v3d.CSG
 import eu.mihosoft.vrl.v3d.Cube
+import eu.mihosoft.vrl.v3d.Isosceles
 
 // code here
-CSG Base = new Cube(	40,// X dimention
-			60,// Y dimention
-			80//  Z dimention
+CSG Base = new Cube(	20,// X dimention
+			20,// Y dimention
+			20//  Z dimention
 			).toCSG()// this converts from the geometry to an object we can work with
+Base = Base.toZMin() 
+		.toXMin() .toYMin()
+CSG Roof = new Isosceles(20,20,15).toCSG()
+		.toXMin() .toYMin() .movez(20)
 
-return Base
+return CSG.unionAll([Base, Roof])
