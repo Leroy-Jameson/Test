54f99b1e5117511d72f4abd87b1a9dd5c823620f
Leroy-Jameson <lgevry@bancroftschool.org>
2025-04-26 09:46:46 -0400

    Update code test.groovy

--- code test.groovy
+++ code test.groovy
@@ -16,4 +16,8 @@ CSG Window = new Cube(	10,// X dimention
 			10//  Z dimention
 			).toCSG()// this converts from the geometry to an object we can work with
 		.movex(20).movey(10).movez(10)
+CSG Window 2 = new cube(   20,// X dimension
+			   20,// y dimention
+			   20,// z dimension
+			   ).toCSG()//
 return CSG.unionAll([Base, Roof]).difference(Window)
